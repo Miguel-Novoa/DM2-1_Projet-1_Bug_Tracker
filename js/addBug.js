a908e1246ceae227f3a38eac9d1d961ed6133467
BManta <novoarodriguezmiguel@gmail.com>
2023-03-01 10:17:23 +0100

    annulation temporaire redirection

--- js/addBug.js
+++ js/addBug.js
@@ -5,5 +5,5 @@ const url = 'http://127.0.0.1:5500';
 const user = localStorage.getItem('user');
 const logoutBtn = document.getElementById('logout');
 
-checkLogged(user, url);
+//checkLogged(user, url);
 logout(logoutBtn, url);
